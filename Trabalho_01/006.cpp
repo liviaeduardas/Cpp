bf2801db47281f17b3438343a486e4f1c578c25e
liviaeduardas <liviaeduardasilveira01@gmail.com>
2024-09-16 16:05:54 -0300

    Exercício 06

---
+++ Trabalho_01/006.cpp
@@ -0,0 +1,35 @@
+/* EXERCÍCIO 06
+ Crie uma função que receba como parâmetro um array e o imprima.
+ Não utilize índices para percorrer o array, apenas aritmética de ponteiros.*/
+ 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void imprimir(int *v)
+{
+	int* pont = v;
+	int i = 0;
+	cout << "--- IMPRIMINDO VETOR ---" << endl;
+	while(pont < v + 5)
+	{
+		cout << "Indice " << i << " : " << *pont << endl;
+		i ++;
+		pont ++;
+	}
+}
+
+int main()
+{
+	int vetor[5];
+	cout << "--- CARREGANDO VETOR ---" << endl;
+	for(int i = 0; i < 5; i++)
+	{
+		cout << "Informe um valor: ";
+		cin >> vetor[i];
+	}
+	cout << endl;
+	
+	imprimir(vetor);
+	return 0;
+}
